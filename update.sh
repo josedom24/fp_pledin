8139c7d3482ca485be5f1986c88c04008706ccfd
José Domingo Muñoz <josedom24@gmail.com>
2022-09-07 20:29:28 +0200

    hola

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 git commit -am "$1"
 git push
-ssh debian@endor.josedomingo.org "sh www/fp_pledin/fp.sh"
+ssh debian@endor.josedomingo.org "export PATH="$HOME/.gems/bin:$PATH";sh www/fp_pledin/fp.sh"
